c47c0d9e8f101fe7d2969737461e8eb6f5c70fb6
neckles422 <124460710+neckles422@users.noreply.github.com>
2023-05-19 16:40:07 +0000

    i dont know what

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import requests 
 
+#print (*args)
+#pass (look this up, not sure what it does)
 
+URL = "https://world.openfoodfacts.org/api/v0/product/737628064502"
 
+response = requests.get(URL) 
+response.raise_for_status() 
+data = response.json()   
+
+print (data)
