17ae18aeec1968b9116536a78fa60007e2772efd
neckles422 <124460710+neckles422@users.noreply.github.com>
2023-05-20 06:19:59 +0000

    finished and added a note for el

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import requests 
+import requests
+import random
 
-#print (*args)
-#pass (look this up, not sure what it does)
+starter = input("welcome to the pokemon suggestion program enter start to recieve a pokemon suggestion:")
 
-URL = "https://world.openfoodfacts.org/api/v0/product/737628064502"
+numb = random.randint(1,1015)
+URL = "https://pokeapi.co/api/v2/pokemon/" + str(numb)
 
-response = requests.get(URL) 
-response.raise_for_status() 
-data = response.json()   
+response = requests.get(URL) # Get data from the URL
 
-print (data)
+response.raise_for_status()  # Throw an exception if the request failed
+
+data = response.json()
+
+if starter == "start":
+  print("you should go find a shiny " + data["name"])
+  print (data["abilities"])
---
+++ readme.md
@@ -0,0 +1,1 @@
+feel free to add changes or base a project off this use of api elliott
